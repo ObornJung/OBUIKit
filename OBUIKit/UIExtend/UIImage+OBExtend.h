1ae173b886e3a68b6f98d478b6650760d02a3120
Oborn.Jung <obornjung@gmail.com>
2015-12-18 14:37:21 +0800

    添加根据color生成image接口

---
+++ OBUIKit/UIExtend/UIImage+OBExtend.h
@@ -0,0 +1,15 @@
+//
+//  UIImage+OBExtend.h
+//  OBUIKit
+//
+//  Created by Oborn.Jung on 12/14/15.
+//  Copyright © 2015 Oborn.Jung. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIImage (OBExtend)
+
++ (UIImage *)imageWithColor:(UIColor *)color size:(CGSize)size;
+
+@end
---
+++ OBUIKit/UIExtend/UIImage+OBExtend.m
@@ -0,0 +1,23 @@
+//
+//  UIImage+OBExtend.m
+//  OBUIKit
+//
+//  Created by Oborn.Jung on 12/14/15.
+//  Copyright © 2015 Oborn.Jung. All rights reserved.
+//
+
+#import "UIImage+OBExtend.h"
+
+@implementation UIImage (OBExtend)
+
++ (UIImage *)imageWithColor:(UIColor *)color size:(CGSize)size {
+    UIGraphicsBeginImageContext(size);
+    CGContextRef ctx = UIGraphicsGetCurrentContext();
+    CGContextSetFillColorWithColor(ctx, color.CGColor);
+    CGContextFillRect(ctx, CGRectMake(0, 0, size.width, size.height));
+    UIImage * image = UIGraphicsGetImageFromCurrentImageContext();
+    UIGraphicsEndImageContext();
+    return image;
+}
+
+@end
